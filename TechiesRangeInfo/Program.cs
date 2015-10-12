23d1a1162421a2ee217c459119fe25291c7db861
JumpAttacker <JumpAttacker@gmail.com>
2015-10-13 01:47:31 +0300

    First release
    v 1.0

---
+++ TechiesRangeInfo/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Ensage;
+using SharpDX;
+
+namespace TechiesRange
+{
+    internal class Program
+    {
+        private static bool _loaded;
+        private static Hero _me;
+        private static Player _player;
+        private static readonly Dictionary<Unit, ParticleEffect> Effects = new Dictionary<Unit, ParticleEffect>();
+        private static readonly Dictionary<Unit, ParticleEffect> Visible = new Dictionary<Unit, ParticleEffect>();
+
+        private static void Main()
+        {
+            Game.OnUpdate += Game_OnUpdate;
+            _loaded = false;
+        }
+        private static void Game_OnUpdate(EventArgs args)
+        {
+            if (!_loaded)
+            {
+                _me = ObjectMgr.LocalHero;
+                _player = ObjectMgr.LocalPlayer;
+                if (!Game.IsInGame || _me == null || _me.ClassID != ClassID.CDOTA_Unit_Hero_Techies)
+                {
+                    return;
+                }
+                _loaded = true;
+                PrintSuccess("> Techies Info loaded!");
+            }
+
+            if (!Game.IsInGame || _me == null || _me.ClassID != ClassID.CDOTA_Unit_Hero_Techies)
+            {
+                _loaded = false;
+                PrintInfo("> Techies Info Unloaded!");
+                return;
+            }
+
+            if (Game.IsPaused)
+            {
+                return;
+            }
+
+            if (_player == null || _player.Team == Team.Observer)
+                return;
+            var bombs = ObjectMgr.GetEntities<Unit>()
+                .Where(
+                    x =>
+                        x.ClassID == ClassID.CDOTA_NPC_TechiesMines && x.Team == _player.Team);
+            foreach (var s in bombs)
+            {
+                HandleEffect(s, s.Spellbook.Spell1 != null);
+            }
+        }
+
+    private static void HandleEffect(Unit unit,bool isRange)
+        {
+            ParticleEffect effect;
+            if (unit.IsAlive)
+            {
+                if (isRange)
+                {
+                    if (!Effects.TryGetValue(unit, out effect))
+                    {
+                        effect = unit.AddParticleEffect(@"particles\ui_mouseactions\range_display.vpcf");
+                        effect.SetControlPoint(1, new Vector3(425, 0, 0));
+                        Effects.Add(unit, effect);
+                    }
+                }
+                if (unit.IsVisibleToEnemies)
+                {
+                    if (Visible.TryGetValue(unit, out effect)) return;
+                    effect = unit.AddParticleEffect("particles/items_fx/aura_shivas.vpcf");
+                    Visible.Add(unit, effect);
+                }
+                else
+                {
+                    if (!Visible.TryGetValue(unit, out effect)) return;
+                    effect.Dispose();
+                    Visible.Remove(unit);
+                }
+            }
+            else
+            {
+                if (isRange)
+                {
+                    if (!Effects.TryGetValue(unit, out effect)) return;
+                    effect.Dispose();
+                    Effects.Remove(unit);
+                }
+                if (!Visible.TryGetValue(unit, out effect)) return;
+                effect.Dispose();
+                Visible.Remove(unit);
+            }
+        }
+
+        public static void PrintInfo(string text, params object[] arguments)
+        {
+            PrintEncolored(text, ConsoleColor.White, arguments);
+        }
+        public static void PrintSuccess(string text, params object[] arguments)
+        {
+            PrintEncolored(text, ConsoleColor.Green, arguments);
+        }
+        public static void PrintError(string text, params object[] arguments)
+        {
+            PrintEncolored(text, ConsoleColor.Red, arguments);
+        }
+        public static void PrintEncolored(string text, ConsoleColor color, params object[] arguments)
+        {
+            var clr = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.WriteLine(text, arguments);
+            Console.ForegroundColor = clr;
+        }
+    }
+}
