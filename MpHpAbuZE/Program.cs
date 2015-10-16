17e941b2db8c77250fcd59ee3454f22a30a7b2d1
JumpAttacker <JumpAttacker@gmail.com>
2015-10-16 18:26:04 +0300

    v1.1
    !!

--- MpHpAbuZE/Program.cs
+++ MpHpAbuZE/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using Ensage;
 using Ensage.Common;
 using Ensage.Common.Extensions;
@@ -17,9 +18,9 @@ namespace MpHpAbuZE
         private static bool _loaded;
         private static Hero _me;
         private static Player _player;
-        private const int TangoRange = 450;
-        private const int QbladeRange = 350;
+        private static byte _stage;
         public static bool Drop { get; set; }
+        private static Vector3 _oldPos;
         #endregion
 
         #region Methods
@@ -32,11 +33,12 @@ namespace MpHpAbuZE
         }
         private static void Game_OnWndProc(WndEventArgs args)
         {
-            if (args.WParam != 'V' || Game.IsChatOpen)
+            if (args.WParam != 'V' || Game.IsChatOpen || args.Msg == (ulong)Utils.WindowsMessages.WM_KEYUP)
             {
                 return;
             }
-            Drop = args.Msg != (ulong) Utils.WindowsMessages.WM_KEYUP;
+            Drop = true;
+            Game.ExecuteCommand(string.Format("dota_player_units_auto_attack_after_spell {0}",Drop?0:1));
             //PrintInfo(Drop.ToString());
         }
 
@@ -66,50 +68,113 @@ namespace MpHpAbuZE
                 return;
             }
 
-            if (_player == null || _player.Team == Team.Observer || !Drop)
+            if (_player == null || _player.Team == Team.Observer)
                 return;
-            var items = _me.Inventory.Items.Where(
-                    x =>
-                        x.Name != "item_soul_ring" && x.Name != "item_arcane_boots");
+            if (!Drop)
+            {
+                _oldPos = _me.NetworkPosition;
+                return;
+            }
             var inInvis = _me.IsInvisible();
             var inStun = _me.IsStunned();
             var isChannel = _me.IsChanneling();
-            var arcaneBoots = _me.Inventory.Items.FirstOrDefault(x => x.Name == "item_arcane_boots");
-            var soulRing = _me.Inventory.Items.FirstOrDefault(x => x.Name == "item_soul_ring");
+            var bottle = _me.Inventory.Items.FirstOrDefault(x => x.Name == "item_bottle");
             if (inStun || inInvis || isChannel) return;
-            //PrintError("-----------------");
-            foreach (var s in items)
+            if (bottle != null)
             {
-                _me.DropItem(s,_me.NetworkPosition);
+                var isMoving = _oldPos != _me.NetworkPosition;
+                if (isMoving)
+                {
+                    Drop = false;
+                    _stage = 1;
+                }
             }
-            if (arcaneBoots != null && arcaneBoots.CanBeCasted())
+            switch (_stage)
             {
-                arcaneBoots.UseAbility();
-                _me.DropItem(arcaneBoots, _me.NetworkPosition);
+                case 0:
+                    if (!Utils.SleepCheck("cd")) break;
+                    var items = _me.Inventory.Items.Where(
+                        x =>
+                            x.Name != "item_soul_ring" && x.Name != "item_arcane_boots" && x.Name != "item_bottle" &&
+                            x.Name != "item_magic_stick" && x.Name != "item_magic_wand").ToList();
+                    var arcaneBoots = _me.Inventory.Items.FirstOrDefault(x => x.Name == "item_arcane_boots");
+                    var soulRing = _me.Inventory.Items.FirstOrDefault(x => x.Name == "item_soul_ring");
+                    
+	                var stick = _me.Inventory.Items.FirstOrDefault(x => x.Name == "item_magic_stick");
+                    var wand = _me.Inventory.Items.FirstOrDefault(x => x.Name == "item_magic_wand");
+                    foreach (var s in items)
+                    {
+                        _me.DropItem(s, _me.NetworkPosition);
+                    }
+                    if (stick != null)
+                    {
+                        if (stick.CanBeCasted())
+                        {
+                            stick.UseAbility();
+                        }
+                        _me.DropItem(stick, _me.NetworkPosition);
+                    }
+                    if (wand != null)
+                    {
+                        if (wand.CanBeCasted())
+                        {
+                            wand.UseAbility();
+                        }
+                        _me.DropItem(wand, _me.NetworkPosition);
+                    }
+                    if (arcaneBoots != null)
+                    {
+                        if (arcaneBoots.CanBeCasted())
+                            arcaneBoots.UseAbility();
+                        _me.DropItem(arcaneBoots, _me.NetworkPosition);
+                    }
+                    if (soulRing != null && soulRing.CanBeCasted())
+                    {
+                        soulRing.UseAbility();
+                    }
+                    /*PrintInfo("------------------------");
+                    foreach (var x in _me.Modifiers)
+                    {
+                        PrintInfo("Modifiers: "+x.Name);
+                    }*/
+                    if (bottle != null/* && bottle.CurrentCharges > 0 && bottle.CurrentCharges<=3*/)
+                    {
+                        if (bottle.CanBeCasted() &&
+                            _me.Modifiers.All(x => x.Name != "modifier_bottle_regeneration"))
+                        {
+                            bottle.UseAbility();
+                            Utils.Sleep(1000, "cd");
+                        }
+                    }
+                    else
+                    {
+                        _stage = 1;
+                    }
+                    Utils.Sleep(200, "w84cast");
+                    break;
+                case 1:
+                    if (!Utils.SleepCheck("w84cast")) break;
+                    var forPick = ObjectMgr.GetEntities<PhysicalItem>().Where(
+                        x =>
+                            x.Distance2D(_me.NetworkPosition) <= 250).ToList();
+                    foreach (var s in forPick)
+                    {
+                        _me.PickUpItem(s);
+                    }
+                    Utils.Sleep(1000, "cd");
+                    _stage = 0;
+                    Drop = false;
+                    Game.ExecuteCommand(string.Format("dota_player_units_auto_attack_after_spell {0}", Drop ? 0 : 1));
+                    break;
+                default:
+                    PrintError(".!.");
+                    break;
             }
-            if (soulRing != null && soulRing.CanBeCasted())
-            {
-                soulRing.UseAbility();
-            }
-            var forPick = ObjectMgr.GetEntities<PhysicalItem>();/*.Where(
-                x =>
-                    x.Distance2D(_me.NetworkPosition) <= 150);*/
-            PrintInfo(forPick.Count().ToString());
-            foreach (var s in forPick)
-            {
-                PrintInfo(s.Name);
-                _me.PickUpItem(s);
-            }
-            Utils.Sleep(250, _me.Handle.ToString());
         }
 
         #endregion
 
         #region Helpers
-        private static float GetDistance2D(Vector3 p1, Vector3 p2)
-        {
-            return (float)Math.Sqrt(Math.Pow(p1.X - p2.X, 2) + Math.Pow(p1.Y - p2.Y, 2));
-        }
         public static void PrintInfo(string text, params object[] arguments)
         {
             PrintEncolored(text, ConsoleColor.White, arguments);
