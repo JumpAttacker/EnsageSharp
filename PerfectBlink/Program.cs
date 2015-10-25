519800162731fd6a8f344a609a4a352b326ab60c
JumpAttacker <JumpAttacker@gmail.com>
2015-10-26 00:14:06 +0300

    v 1.0
    !!!

---
+++ PerfectBlink/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Ensage;
+using Ensage.Common.Extensions;
+using SharpDX;
+
+namespace PerfectBlink
+{
+    class Program
+    {
+        private const string Ver = "1.0";
+
+        static void Main()
+        {
+            Player.OnExecuteOrder += Player_OnExecuteAction;
+            PrintSuccess(string.Format("> Perfect Blink Loaded v{0}", Ver));
+        }
+        static void Player_OnExecuteAction(Player sender, ExecuteOrderEventArgs args)
+        {
+            if (args.Order != Order.AbilityLocation) return;
+            if (args.Ability.Name != "item_blink") return;
+            var me = ObjectMgr.LocalHero;
+            if (!(me.Distance2D(args.TargetPosition) > 1200)) return;
+            var tpos = me.Position;
+            var a = tpos.ToVector2().FindAngleBetween(args.TargetPosition.ToVector2(), true);
+            var p = new Vector3(
+                tpos.X + 1150 * (float)Math.Cos(a),
+                tpos.Y + 1150 * (float)Math.Sin(a),
+                100);
+            args.Ability.UseAbility(p);
+            args.Process = false;
+        }
+        private static void PrintInfo(string text, params object[] arguments)
+        {
+            PrintEncolored(text, ConsoleColor.White, arguments);
+        }
+
+        private static void PrintSuccess(string text, params object[] arguments)
+        {
+            PrintEncolored(text, ConsoleColor.Green, arguments);
+        }
+
+        // ReSharper disable once UnusedMember.Local
+        private static void PrintError(string text, params object[] arguments)
+        {
+            PrintEncolored(text, ConsoleColor.Red, arguments);
+        }
+
+        private static void PrintEncolored(string text, ConsoleColor color, params object[] arguments)
+        {
+            var clr = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.WriteLine(text, arguments);
+            Console.ForegroundColor = clr;
+        }
+    }
+}
---
+++ PerfectBlink/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("PerfectBlink")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PerfectBlink")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("a705c1a0-4d98-4cda-8fba-9892c06065a5")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
