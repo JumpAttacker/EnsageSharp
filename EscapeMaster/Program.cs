ad2366ff31ec18a3d0b6853acb73ce8ccbd451ce
JumpAttacker <JumpAttacker@gmail.com>
2015-12-13 11:21:42 +0300

    v 1.0

---
+++ EscapeMaster/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Ensage;
+using Ensage.Common;
+using Ensage.Common.Extensions;
+using Ensage.Common.Menu;
+
+namespace EscapeMaster
+{
+    internal static class Program
+    {
+        private static bool _loaded;
+        private static readonly Menu Menu = new Menu("Escape Master", "escaper", true);
+        private static bool _isIn;
+        private static readonly List<string> ModifierList = new List<string>
+        {
+            "modifier_kunkka_x_marks_the_spot"/*,
+            "modifier_disruptor_glimpse"*/
+        };
+
+        private static Item _dagger, _eul, _forceStaff;
+
+        private static void Main()
+        {
+            Game.OnUpdate += Game_OnUpdate;
+            //Drawing.OnDraw += Drawing_OnDraw;
+            var dict=new Dictionary<string,bool>
+            {
+                {"item_blink",true},
+                {"item_force_staff",true},
+                {"item_cyclone",true}
+            };
+            Menu.AddItem(new MenuItem("isActive", "Is Active").SetValue(true));
+            Menu.AddItem(new MenuItem("items", "Items:").SetValue(new AbilityToggler(dict)));
+
+            Menu.AddToMainMenu();
+        }
+
+/*
+        private static void Drawing_OnDraw(EventArgs args)
+        {
+            if (!_loaded) return;
+
+        }
+*/
+
+        private static void Game_OnUpdate(EventArgs args)
+        {
+            var me = ObjectMgr.LocalHero;
+
+            if (!_loaded)
+            {
+                if (!Game.IsInGame || me == null)
+                {
+                    return;
+                }
+                _loaded = true;
+                Game.PrintMessage(
+                    "<font face='Comic Sans MS, cursive'><font color='#00aaff'>" + Menu.DisplayName + " By Jumpering" +
+                    " loaded!</font> <font color='#aa0000'>v" + Assembly.GetExecutingAssembly().GetName().Version, MessageType.LogMessage);
+            }
+
+            if (!Game.IsInGame || me == null || !Utils.SleepCheck("kek"))
+            {
+                _loaded = false;
+                return;
+            }
+            if (!me.IsAlive || Game.IsPaused || !Menu.Item("isActive").GetValue<bool>()) return;
+
+            /*foreach (var m in me.Modifiers.AsEnumerable())
+            {
+                //Game.PrintMessage(m.Name,MessageType.ChatMessage);
+            }*/
+
+            var mod = me.Modifiers.FirstOrDefault(x => ModifierList.Contains(x.Name));
+            if (_eul == null || !_eul.IsValid)
+            {
+                _eul=me.FindItem("item_cyclone");
+            }
+            if (mod!=null && !me.IsInvul())
+            {
+                var kun = ObjectMgr.GetEntities<Hero>().FirstOrDefault(x => x.ClassID == ClassID.CDOTA_Unit_Hero_Kunkka && x.IsVisible && x.IsAlive);
+                if (kun != null)
+                {
+                    var spell = kun.FindSpell("kunkka_return");
+                    if (spell != null && spell.IsInAbilityPhase)
+                    {
+                        if (_eul != null && _eul.CanBeCasted() && Menu.Item("items").GetValue<AbilityToggler>().IsEnabled(_eul.Name))
+                        {
+                            _eul.UseAbility(me);
+                            Utils.Sleep(250, "kek");
+                            _isIn = false;
+                            return;
+                        }
+                    }
+                }
+                if (mod.RemainingTime <= 0.2)
+                {
+                    if (_eul != null && _eul.CanBeCasted() && Menu.Item("items").GetValue<AbilityToggler>().IsEnabled(_eul.Name))
+                    {
+                        _eul.UseAbility(me);
+                        Utils.Sleep(250, "kek");
+                        _isIn = false;
+                        return;
+                    }
+                }
+                _isIn = true;
+                return;
+            }
+            if (!_isIn) return;
+            if (_dagger == null || !_dagger.IsValid)
+            {
+                _dagger = me.FindItem("item_blink");
+            }
+            if (_forceStaff == null || !_forceStaff.IsValid)
+            {
+                _forceStaff = me.FindItem("item_force_staff");
+            }
+            
+            if (_dagger != null && _dagger.CanBeCasted() && Menu.Item("items").GetValue<AbilityToggler>().IsEnabled(_dagger.Name))
+            {
+                _dagger.UseAbility(Game.MousePosition);
+                Utils.Sleep(250, "kek");
+            }
+            else if (_forceStaff != null && _forceStaff.CanBeCasted() && Menu.Item("items").GetValue<AbilityToggler>().IsEnabled(_forceStaff.Name))
+            {
+                _forceStaff.UseAbility(me);
+                Utils.Sleep(250, "kek");
+            }
+            else if (_eul != null && _eul.CanBeCasted() && Menu.Item("items").GetValue<AbilityToggler>().IsEnabled(_eul.Name))
+            {
+                _eul.UseAbility(me);
+                Utils.Sleep(250, "kek");
+            }
+            _isIn = false;
+        }
+    }
+}
---
+++ EscapeMaster/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("EscapeMaster")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EscapeMaster")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("2bad20ff-dc44-4ea4-b8e7-dee3b3c00c21")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
