a687a7bb8d8c8a33012ec1756552ed7d3f31d3f0
JumpAttacker <JumpAttacker@gmail.com>
2016-01-02 16:14:30 +0300

    v 1.3

--- ShowSpawnBox/Program.cs
+++ ShowSpawnBox/Program.cs
@@ -1,14 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
 using Ensage;
 using Ensage.Common;
 using SharpDX;
 
 namespace SpawnBox
 {
-    class Program
+    internal static class Program
     {
-        private const string Ver = "1.2";
         private static bool _loaded;
         private static readonly Dictionary<string, ParticleEffect> Effect = new Dictionary<string, ParticleEffect>();
         private static readonly Dictionary<string, ParticleEffect> Effect2 = new Dictionary<string, ParticleEffect>();
@@ -16,64 +16,21 @@ namespace SpawnBox
         private static readonly Dictionary<string, ParticleEffect> Effect4 = new Dictionary<string, ParticleEffect>();
         private const string EffectPath = @"particles\world_environmental_fx\candle_flame.vpcf";
         private static readonly int[,] Spots = {
-            {2240, -4288, 3776, -5312}, {2688, -2944, 3776, -4096},
-            {1088, -3200, 2304, -4544}, {-3530, 768, -2560, -256},
-            {-1026, -2368, 62, -3451}, {-1728, -3522, -706, -4478},
-            {-3459, 4928, -2668, 3968}, {-5056, 4352, -3712, 3264},
-            {3390, -105, 4739, -1102}, {-1921, 3138, -964, 2308},
-            {-832, 4098, -3, 3203}, {447, 3778, 1659, 2822}
+            {2690, -4409, 3529, -5248}, {3936, -3277, 5007, -4431}, 
+			{1088, -3200, 2303, -4543}, {-3307, 383, -2564, -413},
+			{-1023, -2728, 63, -3455}, {-2227, -3968, -1463, -4648},
+			{-4383, 1295, -3136, 400}, {3344, 942, 4719, 7},
+			{-3455, 4927, -2688, 3968}, {-4955, 4071, -3712, 3264}, 
+			{3456, -384, 4543, -1151}, {-1967, 3135, -960, 2176},
+			{-831, 4095, 0, 3200}, {448, 3775, 1663, 2816}
         };
 
-        private static void Main(string[] args)
+        private static void Main()
         {
             _loaded = false;
             Game.OnUpdate += Game_OnUpdate;
-            //Drawing.OnDraw += Drawing_OnDraw;
-            //Drawing.OnEndScene += Drawing_OnEndScene;
         }
 
-        private static void Drawing_OnEndScene(EventArgs args)
-        {
-            //Drawing.DrawRect(new Vector2(0,0),new Vector2(200,200),Color.Red);
-        }
-        private static void Drawing_OnDraw(EventArgs args)
-        {
-            var me = ObjectMgr.LocalHero;
-            if (!_loaded)
-            {
-                if (!Game.IsInGame || me == null)
-                {
-                    return;
-                }
-                _loaded = true;
-                PrintSuccess("> Spawn Box loaded! v" + Ver);
-            }
-            if (!Game.IsInGame || me == null)
-            {
-                _loaded = false;
-                PrintInfo("> Spawn Box unLoaded");
-                return;
-            }
-            if (!Game.IsInGame || !_loaded) return;
-            for (var i = 0; i < 12; i++)
-            {
-                //PrintError(Spots[i, 0].ToString());
-                
-                Vector2 screen;
-
-                if (Drawing.WorldToScreen(new Vector3(Spots[i, 0], Spots[i, 1], 0), out screen))
-                {
-                    Vector2 size;
-                    if (Drawing.WorldToScreen(new Vector3(Spots[i, 2], Spots[i, 3], 0), out size))
-                    {
-                        Drawing.DrawRect(screen, size - screen, Color.AliceBlue);
-                    }
-                //Drawing.DrawRect(screen, size-size,
-                        //Color.Red);//Spots[i, 2] - Spots[i, 0], Spots[i, 3] - Spots[i, 1]
-                   // Drawing.DrawRect(new Vector2(200, 200), new Vector2(200, 200), Color.Red);
-                }
-            }
-        }
         private static void Game_OnUpdate(EventArgs args)
         {
             var me = ObjectMgr.LocalHero;
@@ -84,7 +41,11 @@ namespace SpawnBox
                     return;
                 }
                 _loaded = true;
-                PrintSuccess("> Spawn Box loaded! v" + Ver);
+                Game.PrintMessage(
+                    "<font face='Comic Sans MS, cursive'><font color='#00aaff'>" + "SpawnBox" +
+                    " By Jumpering" +
+                    " loaded!</font> <font color='#aa0000'>v" + Assembly.GetExecutingAssembly().GetName().Version,
+                    MessageType.LogMessage);
             }
             if (!Game.IsInGame || me == null)
             {
@@ -98,10 +59,10 @@ namespace SpawnBox
             }
             if (!Game.IsInGame || !_loaded || !Utils.SleepCheck("Refer")) return;
             Utils.Sleep(500, "Refer");
-            for (var i = 0; i < 12; i++)
+            for (var i = 0; i < 14; i++)
             {
-                var coint1 = Math.Floor(Math.Floor((decimal) (Spots[i, 2] - Spots[i, 0]))/50);
-                var coint2 = Math.Abs(Math.Floor(Math.Floor((decimal) (Spots[i, 3] - Spots[i, 1]))/50));
+                var coint1 = Math.Floor(Math.Floor((decimal)(Spots[i, 2] - Spots[i, 0])) / 50);
+                var coint2 = Math.Abs(Math.Floor(Math.Floor((decimal)(Spots[i, 3] - Spots[i, 1])) / 50));
                 ParticleEffect effect;
                 Vector2 screen;
 
@@ -109,8 +70,8 @@ namespace SpawnBox
                 {
                     for (var a = 1; a < coint1; a++)
                     {
-                        var first = new Vector3(Spots[i, 0] + a*50, Spots[i, 1], 500);
-                        var second = new Vector3(Spots[i, 2] - a*50, Spots[i, 3], 500);
+                        var first = new Vector3(Spots[i, 0] + a * 50, Spots[i, 1], 500);
+                        var second = new Vector3(Spots[i, 2] - a * 50, Spots[i, 3], 500);
                         if (!Effect.ContainsKey(string.Format("{0} / {1}", i, a)))
                         {
                             effect = new ParticleEffect(EffectPath,
@@ -128,8 +89,8 @@ namespace SpawnBox
                     }
                     for (var a = 1; a < coint2; a++)
                     {
-                        var first = new Vector3(Spots[i, 0], Spots[i, 1] - a*50, 500);
-                        var second = new Vector3(Spots[i, 2], Spots[i, 3] + a*50, 500);
+                        var first = new Vector3(Spots[i, 0], Spots[i, 1] - a * 50, 500);
+                        var second = new Vector3(Spots[i, 2], Spots[i, 3] + a * 50, 500);
                         if (!Effect3.ContainsKey(string.Format("{0} / {1}", i, a)))
                         {
                             effect = new ParticleEffect(EffectPath,
@@ -179,19 +140,22 @@ namespace SpawnBox
             }
 
         }
-        public static void PrintInfo(string text, params object[] arguments)
+
+        private static void PrintInfo(string text, params object[] arguments)
         {
             PrintEncolored(text, ConsoleColor.White, arguments);
         }
-        public static void PrintSuccess(string text, params object[] arguments)
+
+        private static void PrintSuccess(string text, params object[] arguments)
         {
             PrintEncolored(text, ConsoleColor.Green, arguments);
         }
-        public static void PrintError(string text, params object[] arguments)
+        private static void PrintError(string text, params object[] arguments)
         {
             PrintEncolored(text, ConsoleColor.Red, arguments);
         }
-        public static void PrintEncolored(string text, ConsoleColor color, params object[] arguments)
+
+        private static void PrintEncolored(string text, ConsoleColor color, params object[] arguments)
         {
             var clr = Console.ForegroundColor;
             Console.ForegroundColor = color;
