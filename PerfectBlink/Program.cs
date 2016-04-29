e1275a33fe00940d70fefde99cdff63978bd3048
JumpAttacker <JumpAttacker@gmail.com>
2016-04-30 00:56:08 +0300

    v 1.0.5

--- PerfectBlink/Program.cs
+++ PerfectBlink/Program.cs
@@ -14,6 +14,7 @@ namespace PerfectBlink
         private static void Main()
         {
             Player.OnExecuteOrder += Player_OnExecuteAction;
+            Game.OnUpdate += Game_OnUpdate;
             PrintSuccess(string.Format("> {1} Loaded v{0}", Assembly.GetExecutingAssembly().GetName().Version, Menu.DisplayName));
             Game.PrintMessage(
                     "<font face='Comic Sans MS, cursive'><font color='#00aaff'>" + Menu.DisplayName + " By Jumpering" +
@@ -23,42 +24,79 @@ namespace PerfectBlink
             Menu.AddToMainMenu();
         }
 
-        private static void Player_OnExecuteAction(Player sender, ExecuteOrderEventArgs args)
+        private static void Game_OnUpdate(EventArgs args)
         {
             if (!Menu.Item("PB.Enable").GetValue<bool>()) return;
-            if (args.Order == Order.AbilityTarget || args.Order == Order.AbilityLocation)
-                if (args.Ability.Name == "item_tpscroll" || args.Ability.Name == "item_travel_boots" ||
-                    args.Ability.Name == "item_travel_boots_2")
-                    TpPos = args.TargetPosition;
-                    
-            if (args.Order != Order.AbilityLocation) return;
-
-            if (args.Ability.Name != "item_blink") return;
-            var me = args.Entities.FirstOrDefault() as Hero;//ObjectMgr.LocalHero);
-            if (me==null) return;
-            var safeRange = me.FindItem("item_aether_lens") == null ? 1200 : 1400;
-            if (!(me.Distance2D(args.TargetPosition) > safeRange))
-                return;
-            var tpos = me.Position;
-            var a = tpos.ToVector2().FindAngleBetween(args.TargetPosition.ToVector2(), true);
-            
-            safeRange -= (int)me.HullRadius;
+            if (!_shouldCheckForModifier || _me==null ||!_me.IsValid) return;
+            var mod = _me.HasModifier("modifier_teleporting");
+            if (mod) return;
+            _shouldCheckForModifier = false;
+            var safeRange = _me.FindItem("item_aether_lens") == null ? 1200 : 1400;
+            var tpos = _me.Position;
+            var a = tpos.ToVector2().FindAngleBetween(_mySelectedPos, true);
+            safeRange -= (int)_me.HullRadius;
             var p = new Vector3(
                 tpos.X + safeRange * (float)Math.Cos(a),
                 tpos.Y + safeRange * (float)Math.Sin(a),
                 100);
-            if (me.Modifiers.Any(x => x.Name == "modifier_teleporting"))
+            _myAbility.UseAbility(p);
+        }
+
+        private static Hero _me;
+        private static Ability _myAbility;
+        private static Vector2 _mySelectedPos;
+        private static bool _shouldCheckForModifier;
+
+        private static void Player_OnExecuteAction(Player sender, ExecuteOrderEventArgs args)
+        {
+            if (!Menu.Item("PB.Enable").GetValue<bool>()) return;
+            /*if (args.Order == Order.AbilityTarget || args.Order == Order.AbilityLocation)
+                if (args.Ability.Name == "item_tpscroll" || args.Ability.Name == "item_travel_boots" ||
+                    args.Ability.Name == "item_travel_boots_2")
+                    TpPos = args.TargetPosition;*/
+            if (args.Order == Order.Stop || args.Order == Order.Hold)
+                _shouldCheckForModifier = false;
+            if (args.Order != Order.AbilityLocation) return;
+            if (args.Ability.Name != "item_blink") return;
+            _me = args.Entities.FirstOrDefault() as Hero;//ObjectMgr.LocalHero);
+            if (_me==null) return;
+            var safeRange = _me.FindItem("item_aether_lens") == null ? 1200 : 1400;
+            if (!(_me.Distance2D(args.TargetPosition) > safeRange))
+                return;
+            var tpos = _me.Position;
+            var a = tpos.ToVector2().FindAngleBetween(args.TargetPosition.ToVector2(), true);
+            
+            safeRange -= (int)_me.HullRadius;
+            var p = new Vector3(
+                tpos.X + safeRange * (float)Math.Cos(a),
+                tpos.Y + safeRange * (float)Math.Sin(a),
+                100);
+            if (_me.HasModifier("modifier_teleporting"))
             {
-                tpos = TpPos;
-                a = tpos.ToVector2().FindAngleBetween(args.TargetPosition.ToVector2(), true);
-                p = new Vector3(
-                    tpos.X + safeRange * (float)Math.Cos(a),
-                    tpos.Y + safeRange * (float)Math.Sin(a),
-                    100);
+                _shouldCheckForModifier = true;
+                _myAbility = args.Ability;
+                args.Process = false;
+                _mySelectedPos = args.TargetPosition.ToVector2();
+                /*DelayAction.Add(new DelayActionItem((int) _me.FindModifier("modifier_teleporting").RemainingTime*1000+750, () =>
+                {
+                    tpos = _me.Position;;//TpPos;
+                    a = tpos.ToVector2().FindAngleBetween(args.TargetPosition.ToVector2(), true);
+                    p = new Vector3(
+                        tpos.X + safeRange * (float)Math.Cos(a),
+                        tpos.Y + safeRange * (float)Math.Sin(a),
+                        100);
+                    Print("extra action");
+                    
+                    args.Ability.UseAbility(p);
+                }, CancellationToken.None));*/
+                return;
             }
-            args.Ability.UseAbility(p,me.IsChanneling());
+            _shouldCheckForModifier = false;
+            args.Ability.UseAbility(p,_me.IsChanneling());
             args.Process = false;
         }
+
+        
 
         private static Vector3 TpPos { get; set; }
 
@@ -85,6 +123,11 @@ namespace PerfectBlink
             Console.ForegroundColor = color;
             Console.WriteLine(text, arguments);
             Console.ForegroundColor = clr;
+        }
+
+        private static void Print(string s)
+        {
+            Game.PrintMessage(s,MessageType.ChatMessage);
         }
     }
 }
