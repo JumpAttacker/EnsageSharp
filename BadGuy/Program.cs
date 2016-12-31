97c05e7484ad2b36339074ffac79eb35f0051b19
Jumpering <JumpAttacker@gmail.com>
2016-12-31 16:54:01 +0300

    fix for 7.00

--- BadGuy/Program.cs
+++ BadGuy/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Drawing;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.InteropServices;
@@ -12,6 +11,7 @@ using SharpDX;
 using Color = SharpDX.Color;
 using Menu = Ensage.Common.Menu.Menu;
 using MenuItem = Ensage.Common.Menu.MenuItem;
+// ReSharper disable CompareOfFloatsByEqualityOperator
 
 namespace BadGuy
 {
@@ -19,14 +19,14 @@ namespace BadGuy
     {
         private static readonly Menu Menu = new Menu("Bad Guy","BG",true);
         //private static bool Enable = false;
-        private static Hero MyHero;
+        private static Hero _myHero;
         private static Unit _fountain;
         private static Unit _fountain2;
         private static float _stageX;
         private static float _stageY;
         private static Vector2 _drawingStartPos = new Vector2(42, 809);
         private static Vector2 _drawingEndPos = new Vector2(278, 1061);//270
-        
+        private static bool _init;
 
         [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
         private static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
@@ -46,56 +46,70 @@ namespace BadGuy
         [PermissionSet(SecurityAction.Assert, Unrestricted = true)]
         private static void Main()
         {
-            var herofeeder = new Menu("Hero Feeder", "Hero");
-            herofeeder.AddItem(new MenuItem("Hero.Enable", "Enable Hero feeder").SetValue(false)).DontSave();
-            herofeeder.AddItem(new MenuItem("Hero.Type", "Feed Type:").SetValue(new StringList(new[] { "attack every creep on midlane", "go to the enemy base" })));
-            herofeeder.AddItem(new MenuItem("Hero.Cd", "Order rate").SetValue(new Slider(500, 100, 2000)));
+            if (!_init)
+            {
+                _init = true;
+                var herofeeder = new Menu("Hero Feeder", "Hero");
+                herofeeder.AddItem(new MenuItem("Hero.Enable", "Enable Hero feeder").SetValue(false)).DontSave();
+                herofeeder.AddItem(
+                    new MenuItem("Hero.Type", "Feed Type:").SetValue(
+                        new StringList(new[] {"attack every creep on midlane", "go to the enemy base"})));
+                herofeeder.AddItem(new MenuItem("Hero.Cd", "Order rate").SetValue(new Slider(500, 100, 2000)));
 
-            var courSelection=new Menu("Courier Selection","cour");
-            courSelection.AddItem(new MenuItem("Courier.Blocking.Enable", "Enable Selected Order").SetValue(false)).DontSave();
-            courSelection.AddItem(new MenuItem("Courier.Cd", "Rate").SetValue(new Slider(50, 5, 200)));
-            courSelection.AddItem(new MenuItem("Courier.MaxRange", "Max Range").SetValue(new Slider(500, 0, 2000)));
-            courSelection.AddItem(
-                new MenuItem("Courier.Order", "Courier Order:").SetValue(
-                    new StringList(new[]
-                    {"blocking on base", "go to the enemy base", "move items to stash", "give items to main hero"})));
+                var courSelection = new Menu("Courier Selection", "cour");
+                courSelection.AddItem(new MenuItem("Courier.Blocking.Enable", "Enable Selected Order").SetValue(false))
+                    .DontSave();
+                courSelection.AddItem(new MenuItem("Courier.Cd", "Rate").SetValue(new Slider(50, 5, 200)));
+                courSelection.AddItem(new MenuItem("Courier.MaxRange", "Max Range").SetValue(new Slider(500, 0, 2000)));
+                courSelection.AddItem(
+                    new MenuItem("Courier.Order", "Courier Order:").SetValue(
+                        new StringList(new[]
+                        {"blocking on base", "go to the enemy base", "move items to stash", "give items to main hero"})));
 
-            var laugh = new Menu("laugh Selection", "laugh");
-            laugh.AddItem(new MenuItem("laugh.Enable", "Enable laugh").SetValue(false));
-            laugh.AddItem(new MenuItem("laugh.Cd", "Rate").SetValue(new Slider(20, 20))).SetTooltip("in secs");
-            laugh.AddItem(new MenuItem("laugh.Message", "Print Message on laught").SetValue(false).SetTooltip("only for you"));
+                var laugh = new Menu("laugh Selection", "laugh");
+                laugh.AddItem(new MenuItem("laugh.Enable", "Enable laugh").SetValue(false));
+                laugh.AddItem(new MenuItem("laugh.Cd", "Rate").SetValue(new Slider(20, 20))).SetTooltip("in secs");
+                laugh.AddItem(
+                    new MenuItem("laugh.Message", "Print Message on laught").SetValue(false).SetTooltip("only for you"));
 
-            var drawing = new Menu("Spam Drawing", "spamDrawing");
-            drawing.AddItem(new MenuItem("Drawing.Fully.Enable", "Fully Enable").SetValue(false));
-            drawing.AddItem(new MenuItem("Drawing.info", "Working only with ctrl hotkey").SetFontColor(Color.Red));
-            drawing.AddItem(new MenuItem("Drawing.Enable", "Enable Spam Drawing").SetValue(new KeyBind(0x11, KeyBindType.Press)).SetTooltip("on minimap").DontSave()).ValueChanged += Program_ValueChanged;
-                
-                
-            drawing.AddItem(new MenuItem("Drawing.Speed", "Speed").SetValue(new Slider(1, 1, 10)));
-            drawing.AddItem(new MenuItem("Drawing.Cd", "Rate").SetValue(new Slider(1, 1, 1000)));
+                var drawing = new Menu("Spam Drawing", "spamDrawing");
+                drawing.AddItem(new MenuItem("Drawing.Fully.Enable", "Fully Enable").SetValue(false));
+                drawing.AddItem(new MenuItem("Drawing.info", "Working only with ctrl hotkey").SetFontColor(Color.Red));
+                drawing.AddItem(
+                    new MenuItem("Drawing.Enable", "Enable Spam Drawing").SetValue(new KeyBind(0x11, KeyBindType.Press))
+                        .SetTooltip("on minimap")
+                        .DontSave()).ValueChanged += Program_ValueChanged;
 
 
-            Menu.AddSubMenu(herofeeder);
-            Menu.AddSubMenu(courSelection);
-            Menu.AddSubMenu(laugh);
-            Menu.AddSubMenu(drawing);
-            Menu.AddToMainMenu();
+                drawing.AddItem(new MenuItem("Drawing.Speed", "Speed").SetValue(new Slider(4, 1, 10)));
+                drawing.AddItem(new MenuItem("Drawing.Cd", "Rate").SetValue(new Slider(40, 1, 1000)));
+
+
+                Menu.AddSubMenu(herofeeder);
+                Menu.AddSubMenu(courSelection);
+                Menu.AddSubMenu(laugh);
+                Menu.AddSubMenu(drawing);
+                Menu.AddToMainMenu();
+            }
 
             Events.OnLoad += (sender, args) =>
             {
-                MyHero = ObjectManager.LocalHero;
+                _myHero = ObjectManager.LocalHero;
+                _drawingStartPos = new Vector2(10, 809);
+                _drawingEndPos = new Vector2(270, 1061);
                 Game.OnUpdate+=Game_OnUpdate;
                 Game.PrintMessage(
                     "<font face='Comic Sans MS, cursive'><font color='#00aaff'>" + Menu.DisplayName +
                     " By Jumpering" +
-                    " loaded!</font> <font color='#aa0000'>v" + Assembly.GetExecutingAssembly().GetName().Version,
-                    MessageType.LogMessage);
+                    " loaded!</font> <font color='#aa0000'>v" + Assembly.GetExecutingAssembly().GetName().Version);
                 _fountain = null;
                 _fountain2 = null;
+                
             };
             Events.OnClose += (sender, args) =>
             {
                 Game.OnUpdate -= Game_OnUpdate;
+                _myHero = null;
             };
         }
         [PermissionSet(SecurityAction.Assert, Unrestricted = true)]
@@ -130,21 +144,22 @@ namespace BadGuy
                 if (_fountain2 == null || !_fountain2.IsValid)
                 {
                     _fountain2 = ObjectManager.GetEntities<Unit>()
-                        .FirstOrDefault(x => x.Team != MyHero.Team && x.ClassID == ClassID.CDOTA_Unit_Fountain);
+                        .FirstOrDefault(x => x.Team != _myHero.Team && x.ClassID == ClassID.CDOTA_Unit_Fountain);
+                    return;
                 }
                 var angle = (float)Math.Max(
-                        Math.Abs(MyHero.RotationRad -
-                                 Utils.DegreeToRadian(MyHero.FindAngleBetween(_fountain2.Position))) - 0.20, 0);
+                        Math.Abs(_myHero.RotationRad -
+                                 Utils.DegreeToRadian(_myHero.FindAngleBetween(_fountain2.Position))) - 0.20, 0);
                 // ReSharper disable once CompareOfFloatsByEqualityOperator
                 if (angle > 0)
                 {
                     if (Menu.Item("Hero.Type").GetValue<StringList>().SelectedIndex == 0)
                     {
-                        MyHero.Attack(_fountain2.Position);
+                        _myHero.Attack(_fountain2.Position);
                     }
                     else
                     {
-                        MyHero.Move(_fountain2.Position);
+                        _myHero.Move(_fountain2.Position);
                     }
                     
                 }
@@ -153,16 +168,16 @@ namespace BadGuy
 
             if (Utils.SleepCheck("Courier.Cd") && Menu.Item("Courier.Blocking.Enable").GetValue<bool>())
             {
-                var couriers = ObjectManager.GetEntities<Courier>().Where(x => x.IsAlive && x.Team == MyHero.Team);
+                var couriers = ObjectManager.GetEntities<Courier>().Where(x => x.IsAlive && x.Team == _myHero.Team);
                 if (_fountain == null || !_fountain.IsValid)
                 {
                     _fountain = ObjectManager.GetEntities<Unit>()
-                        .FirstOrDefault(x => x.Team == MyHero.Team && x.ClassID == ClassID.CDOTA_Unit_Fountain);
+                        .FirstOrDefault(x => x.Team == _myHero.Team && x.ClassID == ClassID.CDOTA_Unit_Fountain);
                 }
                 if (_fountain2 == null || !_fountain2.IsValid)
                 {
                     _fountain2 = ObjectManager.GetEntities<Unit>()
-                        .FirstOrDefault(x => x.Team != MyHero.Team && x.ClassID == ClassID.CDOTA_Unit_Fountain);
+                        .FirstOrDefault(x => x.Team != _myHero.Team && x.ClassID == ClassID.CDOTA_Unit_Fountain);
                 }
                 var index = Menu.Item("Courier.Order").GetValue<StringList>().SelectedIndex;
                 var needFountain = index == (int) Orders.BlockOnBace
@@ -185,8 +200,8 @@ namespace BadGuy
                     {
                         if (index == (int) Orders.Stashing)
                         {
-                            //Game.PrintMessage("Any items: " + courier.Inventory.Items.Any(x => Equals(x.Owner, MyHero)),MessageType.ChatMessage);
-                            //if (courier.Inventory.Items.Any(x => Equals(x.Owner, MyHero)))
+                            //Game.PrintMessage("Any items: " + courier.Inventory.Items.Any(x => Equals(x.Owner, _myHero)),MessageType.ChatMessage);
+                            //if (courier.Inventory.Items.Any(x => Equals(x.Owner, _myHero)))
                             courier.Spellbook.SpellE.UseAbility();
                         }
                         else
@@ -195,8 +210,8 @@ namespace BadGuy
                             {
                                 Game.PrintMessage("Item: " + item.Name+" .Owner: "+item.,MessageType.ChatMessage);
                             }*/
-                            //Game.PrintMessage("Any items: " + courier.Inventory.Items.Any(x => Equals(x.Owner, MyHero)), MessageType.ChatMessage);
-                            if (courier.Inventory.Items.Any(/*x => Equals(x.Owner, MyHero)*/))
+                            //Game.PrintMessage("Any items: " + courier.Inventory.Items.Any(x => Equals(x.Owner, _myHero)), MessageType.ChatMessage);
+                            if (courier.Inventory.Items.Any(/*x => Equals(x.Owner, _myHero)*/))
                                 courier.Spellbook.SpellF.UseAbility();
                         }
                         Utils.Sleep(Menu.Item("Courier.Cd").GetValue<Slider>().Value, "Courier.Cd");
@@ -209,7 +224,7 @@ namespace BadGuy
                 Utils.Sleep(Menu.Item("laugh.Cd").GetValue<Slider>().Value*1000, "laugh.Cd");
                 if (Menu.Item("laugh.Message").GetValue<bool>())
                 {
-                    Game.PrintMessage("Laugh!", MessageType.ChatMessage,MyHero.Player);
+                    Game.PrintMessage("Laugh!");
                 }
             }
             if (!Menu.Item("Drawing.Fully.Enable").GetValue<bool>()) return;
@@ -233,7 +248,7 @@ namespace BadGuy
 
         private static void Print(string s)
         {
-            Game.PrintMessage(s,MessageType.ChatMessage);
+            Game.PrintMessage(s);
         }
     }
 }
