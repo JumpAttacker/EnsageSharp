ef1cd2d83222784905f633a87180f7b720eaab82
osmanbonus007 <osmanbonus007@mail.ru>
2017-04-05 15:15:27 +0300

    update

--- OverylayInformationV2/ShowMeMore.cs
+++ OverylayInformationV2/ShowMeMore.cs
@@ -58,14 +58,14 @@ namespace OverlayInformation
 
         public static readonly List<Vector3> ColorList = new List<Vector3>()
         {
-            new Vector3(0.2f, 0.4588236f, 1),  
+            new Vector3(0.2f, 0.4588236f, 1),
             new Vector3(0.4f, 1, 0.7490196f),
             new Vector3(0.7490196f, 0, 0.7490196f),
             new Vector3(0.9529412f, 0.9411765f, 0.04313726f),
-            new Vector3(1, 0.4196078f, 0),
-            new Vector3(0.9960784f, 0.5254902f, 0.7607843f),
+            new Vector3(1, 0.4196079f, 0),
+            new Vector3(0.9960785f, 0.5254902f, 0.7607844f),
             new Vector3(0.6313726f, 0.7058824f, 0.2784314f),
-            new Vector3(0.3960784f, 0.8509804f, 0.9686275f),
+            new Vector3(0.3960785f, 0.8509805f, 0.9686275f),
             new Vector3(0, 0.5137255f, 0.1294118f),
             new Vector3(0.6431373f, 0.4117647f, 0)
         };
