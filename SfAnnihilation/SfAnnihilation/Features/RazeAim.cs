13d4b171ddcedc9861d0c1f5b49d6b02775f529d
Jumpering <JumpAttacker@gmail.com>
2017-11-20 14:10:31 +0300

    fixed kill steal with raze

--- SfAnnihilation/SfAnnihilation/Features/RazeAim.cs
+++ SfAnnihilation/SfAnnihilation/Features/RazeAim.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Linq;
 using Ensage;
+using Ensage.Common;
 using Ensage.Common.AbilityInfo;
 using Ensage.Common.Extensions;
 using Ensage.Common.Objects;
 using Ensage.Common.Objects.UtilityObjects;
+using Ensage.SDK.Extensions;
 using SfAnnihilation.Utils;
 
 namespace SfAnnihilation.Features
@@ -14,6 +16,35 @@ namespace SfAnnihilation.Features
         public static readonly Hero Me = ObjectManager.LocalHero;
         public static readonly Sleeper KillStealer = new Sleeper();
         public static readonly Sleeper Updater = new Sleeper();
+
+        static RazeAim()
+        {
+            Damage = new float[5];
+            for (uint i = 1; i < 5; i++)
+            {
+                Damage[i] = Core.RazeLow.GetAbilitySpecialData("shadowraze_damage", i);
+            }
+            ExtraDamagePerStack = Core.RazeLow.GetAbilitySpecialData("stack_bonus_damage");
+        }
+
+        public static bool WillDie(Hero target)
+        {
+            var health = target.Health + target.HealthRegeneration;
+            var damage = Damage[Core.RazeLow.Level];
+            var modifier = target.GetModifierByName(ModifierStackName);
+            if (modifier != null)
+            {
+                damage += modifier.StackCount * ExtraDamagePerStack;
+            }
+            damage *= 1 - target.MagicDamageResist;
+            return damage > health;
+        }
+
+        public static float ExtraDamagePerStack { get; set; }
+
+        public static float[] Damage { get; set; }
+        public static string ModifierStackName = "modifier_nevermore_shadowraze_counter";
+
         public static void OnUpdate(EventArgs args)
         {
             if (!MenuManager.AimIsActive || Updater.Sleeping || MenuManager.ComboIsActive)
@@ -23,8 +54,8 @@ namespace SfAnnihilation.Features
                     .FirstOrDefault(
                         x =>
                             x.IsAlive && x.IsVisible && x.IsValidRazeTarget() &&
-                            (!MenuManager.AimKillStealOnly ||
-                             AbilityDamage.CalculateDamage(Core.RazeLow, Core.Me, x) > x.Health + x.HealthRegeneration));
+                            (!MenuManager.AimKillStealOnly || WillDie(x)
+                             /*AbilityDamage.CalculateDamage(Core.RazeLow, Core.Me, x) > x.Health + x.HealthRegeneration*/));
             if (target != null)
             {
                 if (!KillStealer.Sleeping && Core.Razes.Any(x=>x.CanBeCasted() && x.CanHit(target, x.GetAbilityDelay() + 50,false)) && !Core.Razes.Any(y => y.IsInAbilityPhase))
