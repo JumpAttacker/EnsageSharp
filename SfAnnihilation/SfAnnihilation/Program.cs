43416efaa1fd6ba003091b540336f8ac5ba913d1
Jumpering <JumpAttacker@gmail.com>
2018-12-16 14:27:16 +0300

    fix

--- SfAnnihilation/SfAnnihilation/Program.cs
+++ SfAnnihilation/SfAnnihilation/Program.cs
@@ -13,7 +13,7 @@ namespace SfAnnihilation
         {
             Events.OnLoad += (sender, args) =>
             {
-                if (ObjectManager.LocalHero.ClassId != ClassId.CDOTA_Unit_Hero_Nevermore)
+                if (ObjectManager.LocalHero.HeroId != HeroId.npc_dota_hero_nevermore)
                     return;
                 MenuManager.Init();
                 DelayAction.Add(250, () =>
