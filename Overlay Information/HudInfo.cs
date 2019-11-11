34cc07aabea49cd874c2cc8201bfbe0a4a7e73cc
ReeePORTs <ReeePORTs@noreply.com>
2019-11-11 22:54:19 +0000

    Add support for ultra wide resolution

--- Overlay Information/HudInfo.cs
+++ Overlay Information/HudInfo.cs
@@ -93,7 +93,22 @@ namespace OverlayInformation
             }
 
             var ratio = Math.Floor((decimal)(ScreenSize.X / ScreenSize.Y * 100));
-            if (ratio == 213)
+            
+            if (ratio == 266)
+            {
+                Console.WriteLine(
+                    @"Your screen resolution is testing have wrong size/position, (" + ratio
+                    + ")");
+      compareWidth = 2450;
+                panelHeroSizeX = 53.28;
+                tinfoHeroDown = 50.96;
+                DireCompare = 2.301;
+                RadiantCompare = 2.745;
+                HpBarHeight = 9;
+                HpBarWidth = 87;
+                HpBarX = 43;
+                HpBarY = 27;
+            }else if (ratio == 213)
             {
                 compareWidth = 1600;
                 panelHeroSizeX = 45.28;
